ba97d2ccfbf2a59e06f1c41472e2d411cbdf69a2
xx <ss@qq.com>
2019-05-05 14:21:42 +0800

    test case

--- tests/FileHelperTest.php
+++ tests/FileHelperTest.php
@@ -18,4 +18,26 @@ class FileHelperTest extends \PHPUnit\Framework\TestCase
         $size = FileHelper::format(2000, 2);
         $this->assertSame('1.95KB',$size);
     }
+
+    public function testDelDir(){
+        $path = __DIR__.'/tmp/test';
+        if(!file_exists($path)){
+            mkdir($path,null, true);
+        }
+
+        $path = __DIR__.'/tmp';
+
+        $rs = FileHelper::delDir($path);
+        $this->assertTrue($rs);
+
+        $rs = FileHelper::delDir($path, true);
+        $this->assertTrue($rs);
+    }
+
+    public function testGetExt(){
+        $str ='/tmp/test.JPG';
+
+        $rs = FileHelper::getExt($str);
+        $this->assertSame('jpg',$rs);
+    }
 }
